410c465ac1384c278a3b62d7c79fd37be7b16b93
Vitalii Borodulin <vitaliiborodulin@yandex.ru>
2020-11-19 23:15:47 +0300

    initial

---
+++ smartgrid.js
@@ -0,0 +1,27 @@
+module.exports = {
+    columns: 12,
+    offset: '40px', //columns
+    container: {
+        maxWidth: '1200px',
+        fields: '50px' // container
+    },
+    breakPoints: {
+        lg: {
+            width: '1100px',
+        },
+        // md: {
+        //     width: '768px'
+        // },
+        // sm: {
+        //     width: '600px',
+        //     fields: '15px'
+        // },
+        // xs: {
+        //     width: '480px'
+        // },
+        // xxs: {
+			  //     width: '320px',
+        // }
+	},
+	detailedCalc: true
+};
